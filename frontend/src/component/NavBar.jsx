e37fff95693473c52925c8b2d7c54c0783a84dc0
shakir hussain <sh0552802@gmail.com>
2026-04-01 23:42:08 +0530

    add NavBar UI component

--- frontend/src/component/NavBar.jsx
+++ frontend/src/component/NavBar.jsx
@@ -3,7 +3,7 @@ const NavBar = () => {
     <>
       <div className="navbar bg-base-300 shadow-sm ">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">toDev</a>
+          <a className="btn btn-ghost text-xl">Devto</a>
         </div>
         <div className="flex gap-2">
           <div className="dropdown dropdown-end mx-5">
